9daf61544c1317cf7c79e8a7b60a17c0e6e83d52
Eddie Marrero Jr <50213623+eddmarre@users.noreply.github.com>
2020-05-28 19:44:34 -0400

    Splash Screen

---
+++ GlitchGarden/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    int currentSceneIndex;
+    [SerializeField] int timeToWait = 4;
+    private void Start()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if (currentSceneIndex == 0)
+        {
+            StartCoroutine(SplashToStartScreen());
+        }
+
+    }
+
+    IEnumerator SplashToStartScreen()
+    {
+        yield return new WaitForSeconds(timeToWait);
+        LoadNextScene();
+    }
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
