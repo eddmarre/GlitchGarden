1798069491ec8fa0371a742d5c43b77a734cfc51
Eddie Marrero Jr <50213623+eddmarre@users.noreply.github.com>
2020-06-03 12:59:12 -0400

    Events in Animations

--- GlitchGarden/Assets/Scripts/Attacker.cs
+++ GlitchGarden/Assets/Scripts/Attacker.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class Attacker : MonoBehaviour
 {
-    [Range(0f, 5f)] [SerializeField] float walkSpeed = 1f;
+    float currentSpeed = 1f;
 
     private void Update()
     {
-        transform.Translate(Vector2.left * walkSpeed * Time.deltaTime);
+        transform.Translate(Vector2.left * currentSpeed * Time.deltaTime);
+    }
+
+    public void SetMovementSpeed(float speed)
+    {
+        currentSpeed = speed;
     }
 }
