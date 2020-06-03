d973e679406800403743bb29aeb4a985b47c6ff6
Eddie Marrero Jr <50213623+eddmarre@users.noreply.github.com>
2020-06-03 12:49:23 -0400

    Enemy Spawner

---
+++ GlitchGarden/Assets/Scripts/AttackerSpawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackerSpawner : MonoBehaviour
+{
+    [SerializeField] Attacker attackerPrefab;
+    [SerializeField] float minSpawnDelay = 1f;
+    [SerializeField] float maxSpawnDelay = 5f;
+    bool spawn = true;
+
+    IEnumerator Start()
+    {
+        while (spawn)
+        {
+            yield return new WaitForSeconds(Random.Range(minSpawnDelay, maxSpawnDelay));
+            SpawnAttacker();
+        }
+    }
+
+    private void SpawnAttacker()
+    {
+        Instantiate(attackerPrefab, transform.position, Quaternion.identity);
+    }
+}
