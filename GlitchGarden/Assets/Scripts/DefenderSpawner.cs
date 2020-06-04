84b843b244a913cf1c1b520f650cf63fe234036c
Eddie Marrero Jr <50213623+eddmarre@users.noreply.github.com>
2020-06-04 13:59:19 -0400

    Defenders spawn where clicked

--- GlitchGarden/Assets/Scripts/DefenderSpawner.cs
+++ GlitchGarden/Assets/Scripts/DefenderSpawner.cs
@@ -7,11 +7,16 @@ public class DefenderSpawner : MonoBehaviour
     [SerializeField] GameObject defender;
     private void OnMouseDown()
     {
-        SpawnDefender();
+        SpawnDefender(GetSquareClicked());
     }
-
-    void SpawnDefender()
+    private Vector2 GetSquareClicked()
     {
-        GameObject newDefender = Instantiate(defender, transform.position, Quaternion.identity) as GameObject;
+        Vector2 clickPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        Vector2 worldPos = Camera.main.ScreenToWorldPoint(clickPos);
+        return worldPos;
+    }
+    void SpawnDefender(Vector2 worldPos)
+    {
+        GameObject newDefender = Instantiate(defender, worldPos, Quaternion.identity) as GameObject;
     }
 }
