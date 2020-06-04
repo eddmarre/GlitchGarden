f999c771fce3485e001a3abba108b7d6ed9c9512
Eddie Marrero Jr <50213623+eddmarre@users.noreply.github.com>
2020-06-04 13:43:25 -0400

    Enemy Health, Damage, & DeathVFX

---
+++ GlitchGarden/Assets/Scripts/Health.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] float health = 100f;
+    [SerializeField] GameObject deathVFX;
+
+    public void DealDamage(float damage)
+    {
+        health -= damage;
+        DeathHandle();
+    }
+    private void DeathHandle()
+    {
+        if (health <= 0)
+        {
+            TriggerDeathVFX();
+            Destroy(gameObject);
+        }
+    }
+
+    private void TriggerDeathVFX()
+    {
+        if (!deathVFX)
+        {
+            return;
+        }
+        var particles = Instantiate(deathVFX, transform.position, Quaternion.identity);
+        Destroy(particles, 1f);
+
+    }
+}
--- GlitchGarden/Assets/Scripts/Projectile.cs
+++ GlitchGarden/Assets/Scripts/Projectile.cs
@@ -5,8 +5,28 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     [SerializeField] float speed = 1f;
+    [SerializeField] float projectileDamage = 100f;
+    
+
     private void Update()
     {
         transform.Translate(Vector2.right * speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+
+
+        var health = other.GetComponent<Health>();
+        var attacker = other.GetComponent<Attacker>();
+
+        if (attacker && health)
+        {
+            health.DealDamage(projectileDamage);
+            Destroy(gameObject);
+        }
+
+
+
     }
 }
