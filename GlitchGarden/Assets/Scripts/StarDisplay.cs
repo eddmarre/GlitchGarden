6af5449fc6c2c7e3aa41a25ca318866176bb1ddf
Eddie Marrero Jr <50213623+eddmarre@users.noreply.github.com>
2020-06-05 13:37:54 -0400

    Star Resource System

---
+++ GlitchGarden/Assets/Scripts/StarDisplay.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StarDisplay : MonoBehaviour
+{
+    [SerializeField] int stars = 100;
+    Text startText;
+
+    private void Start()
+    {
+        startText = GetComponent<Text>();
+        UpdateDisplay();
+    }
+
+
+    void UpdateDisplay()
+    {
+        startText.text = stars.ToString();
+    }
+
+    public void AddStars(int amount)
+    {
+        stars += amount;
+        UpdateDisplay();
+    }
+
+    public void SpendingStars(int amount)
+    {
+        if (stars >= amount)
+        {
+            stars -= amount;
+            UpdateDisplay();
+        }
+    }
+}
