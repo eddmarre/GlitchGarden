75114961b4d6448ccffda99aaebd001de6840bd4
Eddie Marrero Jr <50213623+eddmarre@users.noreply.github.com>
2020-06-05 12:43:07 -0400

    Spawn Defender on Grid

--- GlitchGarden/Assets/Scripts/DefenderSpawner.cs
+++ GlitchGarden/Assets/Scripts/DefenderSpawner.cs
@@ -13,10 +13,18 @@ public class DefenderSpawner : MonoBehaviour
     {
         Vector2 clickPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
         Vector2 worldPos = Camera.main.ScreenToWorldPoint(clickPos);
-        return worldPos;
+        Vector2 gridPos = SnapToGrid(worldPos);
+        return gridPos;
     }
-    void SpawnDefender(Vector2 worldPos)
+
+    private Vector2 SnapToGrid(Vector2 rawWorldPos)
     {
-        GameObject newDefender = Instantiate(defender, worldPos, Quaternion.identity) as GameObject;
+        float newX = Mathf.RoundToInt(rawWorldPos.x);
+        float newY = Mathf.RoundToInt(rawWorldPos.y);
+        return new Vector2(newX, newY);
+    }
+    void SpawnDefender(Vector2 RoundedPos)
+    {
+        GameObject newDefender = Instantiate(defender, RoundedPos, Quaternion.identity) as GameObject;
     }
 }
