8265e63db4c6d3c94ce613120abb3d3e0d368c49
Eddie Marrero Jr <50213623+eddmarre@users.noreply.github.com>
2020-06-07 13:36:42 -0400

    Game Economy Built

--- GlitchGarden/Assets/Scripts/DefenderSpawner.cs
+++ GlitchGarden/Assets/Scripts/DefenderSpawner.cs
@@ -7,7 +7,7 @@ public class DefenderSpawner : MonoBehaviour
     Defender defender;
     private void OnMouseDown()
     {
-        SpawnDefender(GetSquareClicked());
+        AttemptToPlaceDefenderAt(GetSquareClicked());
     }
     private Vector2 GetSquareClicked()
     {
@@ -19,6 +19,16 @@ public class DefenderSpawner : MonoBehaviour
     public void SetSelectedDefender(Defender defenderToSelect)
     {
         defender = defenderToSelect;
+    }
+    private void AttemptToPlaceDefenderAt(Vector2 gridPos)
+    {
+        var starDisplay = FindObjectOfType<StarDisplay>();
+        int defenderCost = defender.GetStarCost();
+        if (starDisplay.HaveEnoughStars(defenderCost))
+        {
+            SpawnDefender(gridPos);
+            starDisplay.SpendingStars(defenderCost);
+        }
     }
     private Vector2 SnapToGrid(Vector2 rawWorldPos)
     {
