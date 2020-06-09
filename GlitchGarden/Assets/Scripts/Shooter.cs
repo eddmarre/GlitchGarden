651488e993570888d58dfe65fa006ee00cf20fff
Eddie Marrero Jr <50213623+eddmarre@users.noreply.github.com>
2020-06-09 13:02:14 -0400

    Logic for Shooting when Enemies are in Lane

--- GlitchGarden/Assets/Scripts/Shooter.cs
+++ GlitchGarden/Assets/Scripts/Shooter.cs
@@ -5,9 +5,53 @@ using UnityEngine;
 public class Shooter : MonoBehaviour
 {
     [SerializeField] GameObject projectile, gun;
+    AttackerSpawner myLaneSpawner;
+
+    private void Start()
+    {
+        SetLaneSpawner();
+    }
+    private void Update()
+    {
+        if (isAttackerInLane())
+        {
+            Debug.Log("shoot");
+            //TODO change animation state to shooting
+        }
+        else
+        {
+            Debug.Log("Wait");
+            //TODO change animation state to idle
+        }
+    }
+
+    void SetLaneSpawner()
+    {
+        AttackerSpawner[] spawners = FindObjectsOfType<AttackerSpawner>();
+
+        foreach (AttackerSpawner Spawner in spawners)
+        {
+            bool IsCloseEnough = (Mathf.Abs(Spawner.transform.position.y - transform.position.y) <= Mathf.Epsilon);
+            if (IsCloseEnough)
+            {
+                myLaneSpawner = Spawner;
+            }
+        }
+    }
+
+    private bool isAttackerInLane()
+    {
+        if (myLaneSpawner.transform.childCount <= 0)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
     public void Fire()
     {
         Instantiate(projectile, gun.transform.position, Quaternion.identity);
-        return;
     }
 }
