5002b0acc7f23db8c9a487a9726782fd06054c47
Eddie Marrero Jr <50213623+eddmarre@users.noreply.github.com>
2020-06-17 12:54:28 -0400

    Timer with a Slider

---
+++ GlitchGarden/Assets/Scripts/GameTimer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameTimer : MonoBehaviour
+{
+
+    [Tooltip("In Seconds")] [SerializeField] float levelTime = 10;
+
+    private void Update()
+    {
+        GetComponent<Slider>().value = Time.timeSinceLevelLoad / levelTime;
+
+
+        bool timerFinished = (Time.timeSinceLevelLoad >= levelTime);
+        if (timerFinished)
+        {
+            Debug.Log("Level timer expired");
+        }
+    }
+}
