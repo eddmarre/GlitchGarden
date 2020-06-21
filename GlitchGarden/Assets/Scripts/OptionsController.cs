8182ae20f94df6800b9cb4a6dbdee81cceb22a8b
Eddie Marrero Jr <50213623+eddmarre@users.noreply.github.com>
2020-06-21 15:05:43 -0400

    Set and Save Options

---
+++ GlitchGarden/Assets/Scripts/OptionsController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsController : MonoBehaviour
+{
+    [SerializeField] Slider volumeSlider;
+    [SerializeField] float defaultVolume = 0.8f;
+
+    private void Start()
+    {
+        volumeSlider.value = PlayerPrefsController.GetMasterVolume();
+    }
+
+    private void Update()
+    {
+        var musicPlayer = FindObjectOfType<MusicPlayer>();
+        if (musicPlayer)
+        {
+            musicPlayer.SetVolume(volumeSlider.value);
+        }
+        else
+        {
+            Debug.LogWarning("No music player found... Start from splash screen");
+        }
+    }
+
+    public void SaveAndExit()
+    {
+        PlayerPrefsController.SetMasterVolume(volumeSlider.value);
+        FindObjectOfType<LevelLoader>().LoadMainMenu();
+    }
+
+    public void SetDefaults()
+    {
+        volumeSlider.value = defaultVolume;
+    }
+}
